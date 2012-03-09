e159b860b01de268d62cefed03706f036fc57c32
Shawn Crigger <support@s-vizion.com>
2012-03-09 11:48:19 -0500

    Updating more views, fixing errors for Bonfire .6

--- modules/news/controllers/news.php
+++ modules/news/controllers/news.php
@@ -2,6 +2,7 @@
 
 class News extends Front_Controller {
 
+	private $settings;
 	//--------------------------------------------------------------------
 
 	public function __construct()
@@ -10,6 +11,7 @@ class News extends Front_Controller {
 		$this->load->model('news_model');
 		$this->lang->load('news');
 
+		$this->settings = $this->settings_model->select('name,value')->find_all_by('module', 'news');
 	}
 
 	//--------------------------------------------------------------------
@@ -23,86 +25,91 @@ class News extends Front_Controller {
 		Template::render();
 	}
 
-		//--------------------------------------------------------------------
+	//--------------------------------------------------------------------
 
-		public function get_article_list($limit=-1, $offset=0)
+	public function get_article_list($limit=-1, $offset=0)
+	{
+
+		Assets::add_module_css('news','assets/css/news.css');
+
+		$this->load->model('activities/Activity_model', 'activity_model', true);
+
+		if ($limit != -1 && $offset == 0)
 		{
-
-				Assets::add_module_css('news','assets/css/news.css');
-
-				$this->load->model('activities/Activity_model', 'activity_model', true);
-
-				if ($limit != -1 && $offset == 0) {
-								$this->db->limit($limit);
-				} else if ($limit != -1 && $offset > 0) {
-								$this->db->limit($limit, $offset);
-				}
-
-				$this->db->order_by('date', 'desc');
-				$articles = $this->news_model->find_all_by('status_id',3);
-
-				if (!is_array($articles) || !count($articles)) {
-								$this->activity_model->log_activity($this->current_user->id, 'Get Articles: failed. No article were found.', 'news');
-				}
-
-				return $articles;
+			$this->db->limit($limit);
+		} else if ($limit != -1 && $offset > 0)
+		{
+			$this->db->limit($limit, $offset);
 		}
 
-		//--------------------------------------------------------------------
+		$this->db->order_by('date', 'desc');
+		$articles = $this->news_model->find_all_by('status_id',3);
 
-		public function get_articles($limit=-1, $offset=0)
+		if (!is_array($articles) || !count($articles))
 		{
-
-				Assets::add_module_css('news','news.css');
-
-				$this->load->model('activities/Activity_model', 'activity_model', true);
-				$this->load->library('users/auth');
-				$output = '';
-				$articles = $this->news_model->get_articles(true,$limit,$offset);
-
-				if (is_array($articles) && count($articles))
-				{
-						$settings = $this->settings_lib->find_all_by('module','news');
-
-						foreach ($articles as $article)
-						{
-								$article->asset_url = $settings['news.upload_dir_url'];
-								$article->author_name = $this->current_user->username; //($article->author);
-								$output .= $this->load->view('news/index',array('article'=>$article),true);
-						}
-				} else {
-						$output = 'No Articles found.';
-						$this->activity_model->log_activity($this->current_user->id, 'Get Articles: failed. No article were found.', 'news');
-				}
-
-				return $output;
-
+			$this->activity_model->log_activity($this->current_user->id, 'Get Articles: failed. No article were found.', 'news');
 		}
 
-		//--------------------------------------------------------------------
+		return $articles;
+	}
 
-		public function article($article_id = false)
+	//--------------------------------------------------------------------
+
+	public function get_articles($limit=-1, $offset=0)
+	{
+
+		Assets::add_module_css('news','news.css');
+
+		$this->load->model('activities/Activity_model', 'activity_model', true);
+		$this->load->library('users/auth');
+		$output = '';
+		$articles = $this->news_model->get_articles(true,$limit,$offset);
+
+		if (is_array($articles) && count($articles))
 		{
 
-				if ($article_id === false)
-				{
-					return false;
-				}
+			$settings = $this->settings;
 
-				Assets::add_module_css('news','news.css');
-				if (($article = $this->news_model->get_article($article_id)) !== false)
-				{
-						$article->author_name = $this->current_user->username; //($article->author);
-						$settings = $this->settings_lib->find_all_by('module','news');
-						$article->asset_url = $settings['news.upload_dir_url'];
-						Template::set('article',$article);
-				} else {
-						$this->activity_model->log_activity($this->current_user->id, 'Get Article: '. $article_id .' failed. no article found.', 'news');
-				}
-						Template::set_view('news/index');
-						Template::render();
-				}
-
+			foreach ($articles as $article)
+			{
+				$article->asset_url = $settings['news.upload_dir_url'];
+				$article->author_name = $this->current_user->username; //($article->author);
+				$output .= $this->load->view('news/index',array('article'=>$article),true);
+			}
+		} else {
+			$output = 'No Articles found.';
+			$this->activity_model->log_activity($this->current_user->id, 'Get Articles: failed. No article were found.', 'news');
 		}
 
-// End User Admin class
+		return $output;
+
+	}
+
+	//--------------------------------------------------------------------
+
+	public function article($article_id = false)
+	{
+
+		if ($article_id === false)
+		{
+			return false;
+		}
+
+		Assets::add_module_css('news','news.css');
+		if ( ($article = $this->news_model->get_article($article_id)) !== false )
+		{
+			$article->author_name = $this->current_user->username; //($article->author);
+			$settings = $this->settings_lib->find_all_by('module','news');
+			$article->asset_url = $settings['news.upload_dir_url'];
+			Template::set('article',$article);
+		} else {
+			$this->activity_model->log_activity($this->current_user->id, 'Get Article: '. $article_id .' failed. no article found.', 'news');
+		}
+			Template::set_view('news/index');
+			Template::render();
+		}
+
+}
+
+// End News Front End Controller
+
--- modules/news/views/content/published.php
+++ modules/news/views/content/published.php
@@ -1,34 +1,39 @@
-<h2><?php echo lang('us_published_articles'); ?></h2>
-
-<div class="text-right">
+<div class="alert alert-info fade in">
 	<?php echo anchor(SITE_AREA .'/content/news', 'Return to News Article Management'); ?>
 </div>
 
+<div class="admin-box">
+<h3><?php echo lang('us_published_articles'); ?></h3>
+
+	<?php echo form_open($this->uri->uri_string(), 'class="form-horizontal "'); ?>
+
 <?php if (isset($articles) && is_array($articles) && count($articles)) : ?>
-
-<table cellspacing="0">
-    <thead>
-    <tr>
-
-        <th style="width: 40%"><?php echo lang('us_title'); ?></th>
-        <th style="width: 20%"><?php echo lang('us_author'); ?></th>
-        <th style="width: 20%"><?php echo lang('us_date'); ?></th>
-        <th style="width: 20%" class="text-right"><?php echo lang('us_status_change'); ?></th>
+		<table class="table table-striped">
+			<thead>
+				<tr>
+						<th class="column-check"><input class="check-all" type="checkbox" /></th>
+						<th style="width: 40%"><?php echo lang('us_title'); ?></th>
+						<th style="width: 20%"><?php echo lang('us_author'); ?></th>
+						<th style="width: 20%"><?php echo lang('us_date'); ?></th>
+						<th style="width: 20%" class="text-right"><?php echo lang('us_status_change'); ?></th>
     </tr>
     </thead>
+
     <tbody>
-        <?php foreach ($articles as $article) : ?>
+
+				<?php foreach ($articles as $article) : ?>
     <tr>
-        <td><?php echo $article->title ?></td>
-        <td><?php echo $this->auth->username($article->author) ?></td>
-        <td><?php echo $article->date ? date('m/d/Y h:i:s A',$article->date) : '--' ?></td>
-        <td class="text-right">
-            <?php echo anchor(SITE_AREA .'/content/news/set_status/'. $article->id. '/1', lang('us_action_draft'), 'class="ajaxify"') ?> |
-            <?php echo anchor(SITE_AREA .'/content/news/set_status/'. $article->id. '/32', lang('us_action_review'), 'class="ajaxify"') ?> |
-            <?php echo anchor(SITE_AREA .'/content/news/set_status/'. $article->id. '/4', lang('us_action_archive'), 'class="ajaxify"') ?>
-        </td>
+						<td><input type="checkbox" name="checked[]" value="<?php echo $article->id ?>" /></td>
+						<td><?php echo $article->title ?></td>
+						<td><?php echo $article->author ?></td>
+						<td><?php echo $article->date ? date('m/d/Y h:i:s A',$article->date) : '--' ?></td>
+						<td class="text-right">
+								<?php echo anchor(SITE_AREA .'/content/news/set_status/'. $article->id. '/1', lang('us_action_draft'), 'class="ajaxify"') ?> |
+								<?php echo anchor(SITE_AREA .'/content/news/set_status/'. $article->id. '/32', lang('us_action_review'), 'class="ajaxify"') ?> |
+								<?php echo anchor(SITE_AREA .'/content/news/set_status/'. $article->id. '/4', lang('us_action_archive'), 'class="ajaxify"') ?>
+						</td>
     </tr>
-        <?php endforeach; ?>
+				<?php endforeach; ?>
     </tbody>
 </table>
 
