767b9a6e090db49be883d6a7af0cfd00c494c6de
Shawn Crigger <support@s-vizion.com>
2012-03-23 17:04:33 -0400

    Cleaning up left over salmon

--- modules/news/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['module_config'] = array(
-	'description'	=> 'News Articles creation, publishing and management for the Bonfire Library',
-	'author'		=> 'Jeff Fox',
-	'name'			=> 'News',
-	'version'		=> '1.0'
-);
